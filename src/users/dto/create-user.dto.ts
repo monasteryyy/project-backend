42d2b7e3ee5b46bf537384397096777564a1e61e
Julian <ejbarrerar@unal.edu.co>
2026-06-14 21:19:21 -0500

    feat(users): update User DTO and entity with new schema fields

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,27 @@
-export class CreateUserDto {}
+import { IsString, IsEmail, IsOptional, IsBoolean, IsInt, Min } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  name!: string;
+
+  @IsEmail()
+  email!: string;
+
+  @IsString()
+  telephone!: string;
+
+  @IsString()
+  password!: string;
+
+  @IsOptional()
+  @IsString()
+  address?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  verified?: boolean;
+
+  @IsInt()
+  @Min(1)
+  rolId!: number;
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,11 @@
-export class User {}
+export class User {
+  id: number;
+  name: string;
+  email: string;
+  telephone: string;
+  password: string;
+  address: string | null;
+  verified: boolean | null;
+  registerAt: Date;
+  rolId: number;
+}
